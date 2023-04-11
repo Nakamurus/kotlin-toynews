bcfe4470524c4b6fa3036049f564553d1729b384
nakamurus <3872mn@gmail.com>
2023-04-11 12:51:35 +0900

    Add GptApi functionality

--- app/src/main/java/com/android/example/toynewsapplication/data/remote/ApiService.kt
+++ app/src/main/java/com/android/example/toynewsapplication/data/remote/ApiService.kt
@@ -1,10 +1,11 @@
 package com.android.example.toynewsapplication.data.remote
 
+import com.android.example.toynewsapplication.BuildConfig
 import com.android.example.toynewsapplication.data.remote.api.GptApiService
 import com.android.example.toynewsapplication.data.remote.api.NewsApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
+import okhttp3.*
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
@@ -13,8 +14,15 @@ import java.util.concurrent.TimeUnit
 object ApiService {
     private const val BASE_URL_NEWS = "https://newsapi.org/v2/"
     private const val BASE_URL_GPT = "https://api.openai.com/v1/chat/"
+    private const val apiKey = BuildConfig.OPENAI_API_KEY
 
-    private val client = OkHttpClient.Builder()
+    private val newsClient = OkHttpClient.Builder()
+        .readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .build()
+
+    private val gptClient = OkHttpClient.Builder()
+        .authenticator(GptAuthenticator(apiKey))
         .readTimeout(60, TimeUnit.SECONDS)
         .connectTimeout(60, TimeUnit.SECONDS)
         .build()
@@ -27,13 +35,13 @@ object ApiService {
     private val retrofitNews = Retrofit.Builder()
         .baseUrl(BASE_URL_NEWS)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .client(client)
+        .client(newsClient)
         .build()
 
     private val retrofitGpt = Retrofit.Builder()
         .baseUrl(BASE_URL_GPT)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .client(client)
+        .client(gptClient)
         .build()
 
     val newsApiService: NewsApiService by lazy {
@@ -42,5 +50,16 @@ object ApiService {
 
     val gptApiService: GptApiService by lazy {
         retrofitGpt.create(GptApiService::class.java)
+    }
+}
+
+class GptAuthenticator(
+    private val apiKey: String
+): Authenticator {
+    override fun authenticate(route: Route?, response: Response): Request? {
+        if (response.request().header("Authorization") != null) {
+            return null
+        }
+        return response.request().newBuilder().header("Authorization", "Bearer $apiKey").build()
     }
 }
