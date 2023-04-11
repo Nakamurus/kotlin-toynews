1a6e397be449c608d6b6bc3e6118199b972ade5e
nakamurus <3872mn@gmail.com>
2023-04-11 21:40:33 +0900

    add newsdetail UI files

--- app/src/main/java/com/android/example/toynewsapplication/ui/newsdetail/NewsDetailViewModelFactory.kt
+++ app/src/main/java/com/android/example/toynewsapplication/ui/newsdetail/NewsDetailViewModelFactory.kt
@@ -3,12 +3,17 @@ package com.android.example.toynewsapplication.ui.newsdetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.android.example.toynewsapplication.data.domain.News
+import com.android.example.toynewsapplication.data.repository.GptRepository
 
-class NewsDetailViewModelFactory(private val news: News): ViewModelProvider.Factory {
+class NewsDetailViewModelFactory(
+    private val gptRepository: GptRepository,
+    private val news: News): ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(NewsDetailViewModel::class.java)) {
-            return NewsDetailViewModel(news) as T
+            return NewsDetailViewModel(
+                gptRepository,
+                news) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
